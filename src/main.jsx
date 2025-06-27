657a1974a9986e11f84144db3e82aec937b78e94
Ruslan <homastavi@gmail.com>
2025-06-27 23:23:23 +0300

    Корректировка ипорта

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
-import { Routing } from './components/routes/routing';
+import { Routing } from './components/routes/Routing';
 
 import './styles/main.scss';
 
